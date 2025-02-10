b7642e1555814d449e0e41f57843b5c20de41284
yusuf-ozbay <ozbay9985@gmail.com>
2025-02-10 14:17:16 +0300

    BasketItemService ve BasketItemServiceImpl sınıfları oluşturuldu ve gerekli metotlar yazıldı

--- src/main/java/org/example/eticaret/service/BasketItemService.java
+++ src/main/java/org/example/eticaret/service/BasketItemService.java
@@ -1,4 +1,11 @@
 package org.example.eticaret.service;
 
+import org.example.eticaret.entity.BasketItem;
+
 public interface BasketItemService {
+    BasketItem findBasketItemByBasketIdAndProductId(int basketId, int productId);
+    BasketItem save(BasketItem basketItem);
+
+    void delete(int basketItemId);
+
 }
--- src/main/java/org/example/eticaret/service/impl/BasketItemServiceImpl.java
+++ src/main/java/org/example/eticaret/service/impl/BasketItemServiceImpl.java
@@ -1,4 +1,30 @@
 package org.example.eticaret.service.impl;
 
-public class BasketItemServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.example.eticaret.entity.BasketItem;
+import org.example.eticaret.repository.BasketItemRepository;
+import org.example.eticaret.service.BasketItemService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BasketItemServiceImpl implements BasketItemService {
+    private final BasketItemRepository repository;
+    private final ProductServiceImpl productService;
+
+
+    @Override
+    public BasketItem findBasketItemByBasketIdAndProductId(int basketId, int productId) {
+        return null;
+    }
+
+    @Override
+    public BasketItem save(BasketItem basketItem) {
+        return null;
+    }
+
+    @Override
+    public void delete(int basketItemId) {
+
+    }
 }
