19bc96957f4cb3247fca46fcb353cc0044e9eef1
yusuf-ozbay <ozbay9985@gmail.com>
2025-02-11 20:10:06 +0300

    BasketItem ve Basket metotları için convert metotları yazıldı

--- src/main/java/org/example/eticaret/service/mapper/BasketItemServiceMapper.java
+++ src/main/java/org/example/eticaret/service/mapper/BasketItemServiceMapper.java
@@ -1,4 +1,15 @@
 package org.example.eticaret.service.mapper;
 
+import org.example.eticaret.dto.BasketItemDto;
+import org.example.eticaret.entity.BasketItem;
+
 public class BasketItemServiceMapper {
+    public static BasketItemDto toDto(BasketItem basketItem) {
+        return BasketItemDto.builder()
+                .basketItemId(basketItem.getBasketItemId())
+                .basketItemAmount(basketItem.getBasketItemTotalPrice())
+                .count(basketItem.getCount())
+                .product(ProductServiceMapper.toDto(basketItem.getProduct()))
+                .build();
+    }
 }
--- src/main/java/org/example/eticaret/service/mapper/BasketServiceMapper.java
+++ src/main/java/org/example/eticaret/service/mapper/BasketServiceMapper.java
@@ -1,8 +1,12 @@
 package org.example.eticaret.service.mapper;
 
+import org.example.eticaret.dto.BasketDto;
 import org.example.eticaret.dto.ProductDto;
+import org.example.eticaret.entity.Basket;
 import org.example.eticaret.entity.Product;
 import org.example.eticaret.service.CategoryService;
+
+import java.util.stream.Collectors;
 
 public class BasketServiceMapper {
 
@@ -14,6 +18,21 @@ public class BasketServiceMapper {
                 .price(productDto.getPrice())
                 .category(categoryService.getEntity(productDto.getCategoryId()))
                 .stock(productDto.getStock())
+                .build();
+    }
+
+    public static BasketDto toDto(Basket basket){
+
+        return BasketDto.builder()
+                .basketId(basket.getBasketId())
+                .totalPrice(basket.getTotalPrice())
+                .status(basket.getStatus())
+                .customer(CustomerServiceMapper.toDto(basket.getCustomer()))
+                .basketItemList(basket.getBasketItemList()
+                        .stream()
+                        .map(basketItem -> BasketItemServiceMapper.toDto(basketItem))
+                        .collect(Collectors.toList())
+                )
                 .build();
     }
 
