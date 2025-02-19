a03ffdcf1ea42508080e9872eb2b820deb911fbd
yusuf-ozbay <ozbay9985@gmail.com>
2025-02-19 23:07:22 +0300

    BasketController ce BasketRequest.java

--- src/main/java/org/example/eticaret/controller/BasketController.java
+++ src/main/java/org/example/eticaret/controller/BasketController.java
@@ -1,4 +1,42 @@
 package org.example.eticaret.controller;
 
+
+import lombok.RequiredArgsConstructor;
+import org.example.eticaret.dto.BasketDto;
+import org.example.eticaret.request.BasketRequest;
+import org.example.eticaret.response.BasketResponse;
+import org.example.eticaret.service.BasketService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("baskets")
+@RequiredArgsConstructor
 public class BasketController {
+
+    private final BasketService basketService;
+
+        @PostMapping
+        public BasketResponse addProductToBasket(@RequestBody BasketRequest basketRequest){
+            return toResponse(basketService.addProductToBasket(basketRequest.toDto()));
+        }
+
+        @GetMapping("/{customerId}")
+        public BasketResponse getBasketByCustomerId(@PathVariable String customerId){
+            return toResponse(basketService.getBasketByCustomerId(customerId));
+        }
+
+        @DeleteMapping("/{basketItemId}")
+        public String delete(@PathVariable String basketItemId){
+            basketService.removeProductFromBasket(basketItemId);
+            return "Silme işlemi başarılı";
+        }
+
+    public BasketResponse toResponse(BasketDto basketDto) {
+        return BasketResponse.builder()
+                .basketId(basketDto.getBasketId())
+                .totalPrice(basketDto.getTotalPrice())
+                .customerId(basketDto.getCustomer().getCustomerId())
+                .basketItemDtoList(basketDto.getBasketItemList())
+                .build();
+    }
 }
--- src/main/java/org/example/eticaret/request/BasketRequest.java
+++ src/main/java/org/example/eticaret/request/BasketRequest.java
@@ -1,17 +1,34 @@
 package org.example.eticaret.request;
 
 import lombok.*;
+import org.example.eticaret.dto.BasketDto;
+import org.example.eticaret.dto.BasketItemDto;
+import org.example.eticaret.dto.CustomerDto;
+import org.example.eticaret.dto.ProductDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Builder
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Data
 public class BasketRequest {
 
 
-    private Long customerId;
-    private Long productId;
+    private int customerId;
+    private int productId;
     private int count;
 
 
+    public BasketDto toDto(){
+        List<BasketItemDto> dtoList = new ArrayList<>();
+        BasketItemDto dto = BasketItemDto.builder().
+                product(new ProductDto(productId)).
+                build();
+        dto.setCount(count);
+        dtoList.add(dto);
+        return BasketDto.builder()
+                .customer(CustomerDto.builder().customerId(customerId).build())
+                .basketItemList(dtoList)
+                .build();
+    }
 }
